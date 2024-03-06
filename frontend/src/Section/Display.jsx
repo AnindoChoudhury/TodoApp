fe60184eaa28271a83644f8713276e32cc77efae
Firdous <firdousrahaman42@gmail.com>
2024-03-06 18:53:54 +0530

    Fixed Rendering Issue

--- frontend/src/Section/Display.jsx
+++ frontend/src/Section/Display.jsx
@@ -1,78 +1,107 @@
-import React, { useState, useEffect, useContext,useRef} from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import TodoContext from "../context/TodoContext";
 import { Button } from "@/components/ui/button";
 import TopSection from "./TopSection";
 import { Checkbox } from "@/components/ui/checkbox";
 // import {Checkbox} from "@/components/custom-components/Checkbox";
- function NoTodoHeading()
-{
-  const {title,setTitle}=useContext(TodoContext);
-  return(
-    <h2 className="noTodoHeading mt-10 scroll-m-20 pb-2 text-3xl self-start font-semibold tracking-tight transition-colors first:mt-0" style={{marginTop:"2.8rem",marginLeft:"2rem",display:title}}>No Todos Here</h2>
-  )
+function NoTodoHeading() {
+  const { title, setTitle } = useContext(TodoContext);
+  return (
+    <h2
+      className="noTodoHeading mt-10 scroll-m-20 pb-2 text-3xl self-start font-semibold tracking-tight transition-colors first:mt-0"
+      style={{ marginTop: "2.8rem", marginLeft: "2rem", display: title }}
+    >
+      No Todos Here
+    </h2>
+  );
 }
 
 function Display() {
   let [color, setColor] = useState([]);
   // const [check, setCheck] = useState([false]);
-  const { todoCon,setTodoCon,check,title,setTitle,setCheck,setTotalNumberOfTodos,arraySelector,setArraySelector,totalNumberOfTodos} = useContext(TodoContext);
+  const {
+    todoCon,
+    setTodoCon,
+    check,
+    title,
+    setTitle,
+    setCheck,
+    setTotalNumberOfTodos,
+    arraySelector,
+    setArraySelector,
+    totalNumberOfTodos,
+  } = useContext(TodoContext);
 
-  useEffect(()=>
-  {
-    if(todoCon.length)
-    {
-       setTitle("block");
-    }
-    else
-    setTitle("hidden");
+  useEffect(() => {
+    if (todoCon.length) {
+      setTitle("block");
+    } else setTitle("hidden");
     setTotalNumberOfTodos(todoCon.length);
     setArraySelector([...todoCon]);
-  },[todoCon]);
-  
+  }, [todoCon]);
+
   // If there is at least a single todo in the todoCon array
- 
+
   const checkRef = useRef(null);
   return (
+    <div
+      className="display"
+      style={{
+        // display: "flex",
+        // flexDirection: "column",
+        backgroundColor: "white",
+        width: "100%",
+        // alignItems: "center",
+        height: "100vh",
+        overflow: "auto",
+        paddingBottom: "50px",
+      }}
+    >
+      <TopSection />
       <div
-        className="display"
+        className="displaychild"
         style={{
-          // display: "flex",
-          // flexDirection: "column",
-          backgroundColor:"white",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
           width: "100%",
-          // alignItems: "center",
-          height : "100vh",
-          overflow: "auto",
-          paddingBottom: "50px",
+          marginTop: "2.8rem",
         }}
       >
-        <TopSection/>
-    <div className="displaychild" style={{display:"flex",justifyContent:"center",flexDirection:"column",alignItems:"center",width:"100%",marginTop : "2.8rem"}}>
         {arraySelector.map((item, i) => {
-          let todoItemBackgroundColor = i%2===0?"#ECB159":"#F0F3FF";
+          let todoItemBackgroundColor = i % 2 === 0 ? "#ECB159" : "#F0F3FF";
 
-          let borderRadiusOfItem = i===0? "10px 10px 0 0" : (i===todoCon.length-1)? "0 0 10px 10px" : "0 0 0 0";
-         
-          return(
-          <div
-            key={i}
-            style={{
-              width: "90%",
-              backgroundColor:todoItemBackgroundColor,
-              borderRadius : borderRadiusOfItem,
-              padding : "20px",
+          let borderRadiusOfItem =
+            i === 0
+              ? "10px 10px 0 0"
+              : i === todoCon.length - 1
+              ? "0 0 10px 10px"
+              : "0 0 0 0";
 
-            }}
-          >
-            <div className="flex gap-6 items-center"> 
-            <Checkbox id={item.id} ref={checkRef} completed={item.completed}/> 
-            <p className="leading-7">{item.todoTitle}</p>  
+          return (
+            <div
+              key={i}
+              style={{
+                width: "90%",
+                backgroundColor: todoItemBackgroundColor,
+                borderRadius: borderRadiusOfItem,
+                padding: "20px",
+              }}
+            >
+              <div className="flex gap-6 items-center">
+                <Checkbox
+                  ref={checkRef}
+                  props={item}
+                />
+                <p className="leading-7">{item.todoTitle}</p>
+              </div>
             </div>
-          </div>
-        )})}
-        </div>
+          );
+        })}
       </div>
-    );
-};
+    </div>
+  );
+}
 
 export default Display;
--- frontend/src/components/ui/checkbox.jsx
+++ frontend/src/components/ui/checkbox.jsx
@@ -1,39 +1,58 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { useEffect } from "react"
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { Check } from "lucide-react"
-import { useContext } from "react"
-import TodoContext from "@/context/TodoContext"
-import { cn } from "@/lib/utils"
+import * as React from "react";
+import { useEffect } from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "lucide-react";
+import { useContext } from "react";
+import TodoContext from "@/context/TodoContext";
+import { cn } from "@/lib/utils";
 
-const Checkbox = React.forwardRef(({ className, ...props }, ref) => {
-  const {todoCon,completedTodo,setCompletedTodo,checkedStatus,setCheckedStatus} = useContext(TodoContext);
+const Checkbox = React.forwardRef(({ className, props }, ref) => {
+  const {
+    todoCon,
+    setTodoCon,
+    completedTodo,
+    setCompletedTodo,
+    checkedStatus,
+    setCheckedStatus,
+  } = useContext(TodoContext);
 
-  useEffect(()=>
-  {
+  useEffect(() => {
     console.log(completedTodo);
-  },[completedTodo])
-  
-  return(
-  <CheckboxPrimitive.Root
-    onCheckedChange={function()
-    {
-        todoCon[ref.current.id].completed=ref.current.ariaChecked;
-        setCompletedTodo([...todoCon.filter((item)=>(item.completed==="true"))])
-    }}
-    ref={ref}
-    className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=true]:bg-primary data-[state=true]:text-primary-foreground",
-      className
-    )}
-    {...props}>
-    <CheckboxPrimitive.Indicator className={cn("flex items-center justify-center text-current")}>
-      <Check className="h-4 w-4" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
-)})
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+  }, [completedTodo]);
 
-export { Checkbox }
+  useEffect(() => {
+    setCompletedTodo([...todoCon.filter((todo) => todo.completed)]);
+  }, [todoCon]);
+
+  return (
+    <CheckboxPrimitive.Root
+      onCheckedChange={function () {
+        setTodoCon((prev) => {
+          return prev.map((todo) => {
+            return todo.id === props.id
+              ? { ...todo, completed: !todo.completed }
+              : todo;
+          });
+        });
+      }}
+      ref={ref}
+      checked={props.completed}
+      className={cn(
+        "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=true]:bg-primary data-[state=true]:text-primary-foreground",
+        className
+      )}
+      {...props}
+    >
+      <CheckboxPrimitive.Indicator
+        className={cn("flex items-center justify-center text-current")}
+      >
+        <Check className="h-4 w-4" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
+});
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
