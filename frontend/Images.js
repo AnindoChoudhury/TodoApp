c9f72cbceeac5d51d3c1ce748f57c4d355fd8433
AnindoChoudhury <anindo.choudhury2004@gmail.com>
2024-03-08 10:04:44 +0530

    added

--- frontend/Images.js
+++
@@ -1,7 +0,0 @@
-import images from "./Images.js/"
-setTimeout(async ()=>
-{
-    const res = await fetch("https://source.unsplash.com/random?orientation=landscape"); 
-    const data = await res.json();
-    console.log(data);
-},1000);
